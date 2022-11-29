bd3889e1bef33f6b3fab9d0c762ff4ff4f7b6939
Ngatoh <alexanderngatia@gmail.com>
2022-11-29 18:17:17 +0300

    working anonymization

--- anonymizer/admin.py
+++ anonymizer/admin.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+import csv
+from faker import Factory
+from collections import defaultdict
+from django.shortcuts import redirect, render
 from django.contrib import admin
 from .models import Post
 from django.http import HttpResponseRedirect
@@ -6,22 +9,50 @@ from django.http import HttpResponseRedirect
 @admin.register(Post)
 
 class PostAdmin(admin.ModelAdmin):
-    def response_change(self, request, obj, post_url_continue =None):
+    
+    def response_change(self, request, obj):
         if '_generate_values' in request.POST:
 
-            return HttpResponseRedirect("post/change/generate_values.html")
-        return super().response_change(request, obj)
-           #return TemplateResponse('post/chnge/generate_values.html', context {'variable': obj.id})
-           #render(request, template_name='admin/anonymizer/post/generate_values.html')
-           #context={'variable': 'obj.id'}
+            #filename= obj.file.name #"originalfile.csv"
 
+            source= obj.file.path #"".join([MEDIA_URL, filename])
+            target="media/newfile.csv"
+            """
+                The source argument is a path to a CSV file containing data to anonymize,
+                 while target is a path to write the anonymized CSV data to.
+             """
+            with open(source, 'r') as f:
+              with open(target, 'w') as o:
+            # Use the DictReader to easily extract fields
+                   reader = csv.DictReader(f)
+                   writer = csv.DictWriter(o, reader.fieldnames)
 
-    #def detail(request, poll_id):
-       #sreturn render(request, 'polls/detail.html', {'poll': p})
+            # Read and anonymize data, writing to target file.
+                   for row in anonymize_rows(reader):
+                       writer.writerow(row)
+        return super(PostAdmin, self).response_change(request, obj)
+        #return HttpResponseRedirect("/admin/anonymizer/post/")
 
-    #def get_urls(self):
-        #urls = super().get_urls()
-        #pattern = path('generate/<int:id>/', self.admin_site.admin_view(self.response_change), name='preview-pdf')
-       # urls += pattern
-       # return urls
+def anonymize_rows(rows):
+    """
+    Rows is an iterable of dictionaries that contain name and
+    email fields that need to be anonymized.
+    """
+    # Load the faker and its providers
+    faker  = Factory.create()
 
+    # Create mappings of names & emails to faked names & emails.
+    names  = defaultdict(faker.name)
+    emails = defaultdict(faker.email)
+    address = defaultdict(faker.address)
+
+    # Iterate over the rows and yield anonymized rows.
+    for row in rows:
+        # Replace the name and email fields with faked fields.
+        row['name']  = names[row['name']]
+        row['email'] = emails[row['email']]
+        row['address'] = address[row['address']]
+
+        # Yield the row back to the caller
+        yield row
+         
--- media/urls.py
+++ media/urls.py
@@ -1,28 +1,4 @@
-"""anonymize URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('anonymizer.urls')),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+name,email,value,time,ipaddr,address
+James Hinglee,jhinglee@gmail.com,a,1446288248,202.12.32.123
+Nancy Smithfield,unicorns4life@yahoo.com,b,1446288250,67.212.123.201
+J. Hinglee,jhinglee@gmail.com,b,1446288271,202.12.32.123
